576d0f569fe4e3609b67638b741484f934c34ccb
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-06-07 16:53:10 -0500

    BONUS DAY 6 StringBuilder
    I’m a lumberjack and I’m okay.

---
+++ src/lumberjack.java
@@ -0,0 +1,10 @@
+class lumberjack {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("I'm a lumberjack");
+        sb.append(" and I'm okay.");
+
+        System.out.println(sb);
+    }
+}
